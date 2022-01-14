830739d54ef991d25ba85416c1dc30b652ef2952
Zayne Komichi <zaynekomichi@hotmail.com>
2022-01-14 15:13:42 +0200

    created split inventory

--- src/renderer/Pages/Inventory.tsx
+++ src/renderer/Pages/Inventory.tsx
@@ -1,12 +1,14 @@
-import { IoCart, IoClose, IoSearch } from 'react-icons/io5';
+import { IoCalendar, IoCart, IoClose, IoSearch } from 'react-icons/io5';
 import { useState,useEffect } from 'react';
-import { IonAlert } from '@ionic/react';
+import { IonAlert,IonBadge } from '@ionic/react';
 
 import axios from 'axios';
 import '../css/general.css';
 import { serverLink } from 'renderer/links';
 
 const Inventory = ()=>{
+
+const [id,setId] = useState<number>();
 const [objects,setObjects] = useState<any>();
 const [visible,setVisible] = useState(false);
 const [productName,setProductName] = useState('');
@@ -23,10 +25,15 @@ const [returned,setReturned] =useState([]);
 const [displayed, setDisplay] = useState('none');
 const [secVisible,setSecVisible] = useState(false);
 const [expireDate,setExpired] = useState('');
-useEffect(() => {
+const TodaysDate = new Date();
+const [ExpireSecond,setExpire] = useState('')
+const [inventory,setInventory] = useState<number>(1);
+
+const request = (type:number)=>{
   axios.get(`${serverLink}Inventory.php`,{
     params:{
         'getData':1,
+        'type':type
       }
       }).then((response)=>{
         if(response.data !== null || response.data !== []){
@@ -37,6 +44,9 @@ useEffect(() => {
       }).catch((error)=>{
         setErrorMessage("Server Offline");
       })
+}
+useEffect(() => {
+  request(1);
 },[]);
 
 const SearchInventory=()=>{
@@ -58,6 +68,33 @@ const List = () =>{
   if(returned.length !== 0){
     return(<div>
        {returned.map((items:any)=>{
+          let expiryDate:any = new Date(items.ExpireDate);
+          let currentDate:any =new Date();
+          let DateSub = expiryDate-currentDate;
+          if(DateSub<2548931590 && DateSub>0){
+            items.ExpireDate = <p className="badgeText warning" >Expiring On {items.ExpireDate}</p>
+          }
+
+          if(TodaysDate>=new Date(items.ExpireDate)){
+            items.ExpireDate = <p className="badgeText danger" ><IoCalendar/> Expired On {items.ExpireDate}</p>
+          }
+          if(TodaysDate<new Date(items.ExpireDate)){
+            items.ExpireDate = <p className="badgeText primary" ><IoCalendar/> Expiry On {items.ExpireDate}</p>
+          }
+
+
+         if (items.Quantity<=10 && items.Quantity>5){
+           items.Quantity = <p className='badgeText warning'><IoCart/>{items.Quantity}</p>
+         }
+         if (items.Quantity>10){
+          items.Quantity = <p className='badgeText primary'><IoCart/>{items.Quantity}</p>
+        }
+        if (items.Quantity<=5){
+          items.Quantity = <p className='badgeText danger'><IoCart/>{items.Quantity}</p>
+        }
+        // if (items.Ex<=10 && items.Quantity>5){
+        //   items.Quantity = <p className='badgeText warning'><IoCart/>{items.Quantity}</p>
+        // }
          return(
           <div className='FlexInfoInventory' key={items.id}onClick={()=>{
 
@@ -69,6 +106,8 @@ const List = () =>{
             setSigned(items.ReceivedBy);
             setdateSigned(items.ReceiveDate);
             setExpired(items.ExpireDate);
+            setExpire(items.ExpireDate)
+            setId(items.id);
 
           if(visible===false){
             setVisible(true);
@@ -85,7 +124,6 @@ const List = () =>{
             </div>
             <div className='ResultCards'>
               <p>{items.ExpireDate}</p>
-
             </div>
             </div>
 
@@ -106,13 +144,20 @@ const  UpdateData=(data:any)=>{
   if(data.Quantity==='')data={...data, Quantity:quantity}
   if(data.Notes ==='')data={...data, Notes:notes}
   if(data.Provider ==='')data={...data,Provider:provider}
-  if(data.ExpireDate ==='')data={...data, ExpireDate: expireDate}
+  if(data.ExpiryDate ==='')data={...data, ExpiryDate: expireDate}
   if(data.ReceiveDate ==='')data={...data,ReceiveDate:dateSigned}
-  axios.get(`${serverLink}`,{
+  axios.get(`${serverLink}AdminUpdate.php`,{
     params:{
-      
+      'postData':1,
+      data
     }
-  })
+  }).then((response)=>{
+    if(response.data===1){
+      request(1);
+    }
+  }).catch((error)=>{
+    console.log(error)
+  });
 }
 
 
@@ -122,6 +167,10 @@ const  UpdateData=(data:any)=>{
         <div> <h1>Inventory</h1></div>
         <div><input type="text" placeholder='Search Inventory' value={searchData} onChange={(e)=>setSearchData(e.target.value!)}/></div>
         <div><button className='btn btn-block' onClick={()=>{SearchInventory(), setDisplay('block')}}><IoSearch size="20px" color="#00aeff"/></button></div>
+        <div><button className='btn btn-block' onClick={()=>{ request(1)}}>Drugs</button></div>
+        <div><button className='btn btn-block' onClick={()=>{ request(2)}}>Hospital</button></div>
+        <div><button className='btn btn-block' onClick={()=>{ request(3)}}>Kitchen</button></div>
+
       </div>
       <p className='error-message'>{ErrorMessage}</p>
       <div className='Tables'>
@@ -131,6 +180,7 @@ const  UpdateData=(data:any)=>{
           <th>Expiry Date</th>
         <th>Quantity</th>
         <th>Product Name</th>
+        <th>ExpiryDate</th>
           </tr>
         </thead>
         <tbody>
@@ -146,6 +196,8 @@ const  UpdateData=(data:any)=>{
             setSigned(items.ReceivedBy);
             setdateSigned(items.ReceiveDate);
             setExpired(items.ExpireDate);
+            setExpire(items.ExpireDate)
+            setId(items.id);
 
           if(visible===false){
             setVisible(true);
@@ -154,6 +206,7 @@ const  UpdateData=(data:any)=>{
             <td>{items.ExpireDate}</td>
             <td>{items.Quantity}</td>
             <td style={{'textAlign':'left'}}>{items.productName}</td>
+            <td>{items.ExpireDate}</td>
           </tr>
           );
         })}
@@ -164,14 +217,15 @@ const  UpdateData=(data:any)=>{
       isOpen={visible}
       header={productName}
       message={`
-      Item  Code ▼ <br/> ${code} <br/>
-      Item Provider:- ${provider} <br/>
-      Item Quantity:- ${quantity} <br/>
-      Item Signed By:- ${signed} <br/>
-      Date Signed:-${dateSigned}
-      Item Notes ▼  <br/>${notes}<br>
-      <h2>Update ${productName}</h2>
-      <small>Update for the ones you want to change only</small>
+      <br/>Item  Code <br/> ${code} <br/>
+      <br/>Provider:- ${provider} <br/>
+      <br/>Signed By:- ${signed} <br/>
+      <br/>Date signed:- ${dateSigned} <br/>
+      <br/>Item Notes   <br/>${notes}<br>
+      <h3><br/>Update ${productName}</h3>
+      <br/>
+      <small>Update for the ones you want to change only<br/></small>
+      <br/>
       `}
       inputs={[
         {
@@ -223,7 +277,7 @@ const  UpdateData=(data:any)=>{
             type:'date'
           },
           {
-            name:'ExpireDate',
+            name:'ExpiryDate',
             type:'date'
           }
         ]}
@@ -232,7 +286,7 @@ const  UpdateData=(data:any)=>{
             text:'Update',
             role:'update',
             handler:(data)=>{
-              const newData = {...data,...objects};
+              const newData = {...data,...objects, id};
               UpdateData(newData);
               setSecVisible(false);
             }
